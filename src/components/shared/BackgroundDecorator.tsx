86d070148e9e393351cedd6d90b8eba0faaf4275
jurius456 <sf7djfxd8f@privaterelay.appleid.com>
2026-04-10 21:31:32 +0300

    009

--- src/components/shared/BackgroundDecorator.tsx
+++ src/components/shared/BackgroundDecorator.tsx
@@ -1,14 +1,14 @@
 const BackgroundDecorator = () => {
   return (
-    <div className="fixed inset-0 -z-10 overflow-hidden bg-[#FBFBFB] pointer-events-none">
+    <div className="fixed inset-0 -z-10 overflow-hidden bg-white pointer-events-none">
       
       {/* 1. ГЕОМЕТРИЧНА ОСНОВА (Тінь від стіни/вікна) */}
       {/* Використовуємо Skew для створення динаміки */}
       <div className="absolute top-[-10%] left-[-5%] w-[60vw] h-[110vh] bg-zinc-200/40 -skew-x-12 blur-[100px]" />
 
       {/* 2. СВІТЛОВИЙ АКЦЕНТ (Промінь світла) */}
-      {/* Прямокутник, що імітує сонячне світло, яке падає на підлогу */}
-      <div className="absolute top-[20%] right-[10%] w-[40vw] h-[80vh] bg-white shadow-[0_0_150px_rgba(255,255,255,0.8)] rotate-12 blur-[60px]" />
+      {/* У світлій темі це сонячне світло, у темній - підсвітка */}
+      <div className="absolute top-[20%] right-[10%] w-[40vw] h-[80vh] bg-[#FFFFFF] shadow-[0_0_150px_rgba(255,255,255,0.8)] dark:bg-[#FFFFFF]/5 dark:shadow-[0_0_150px_rgba(255,255,255,0.05)] rotate-12 blur-[60px]" />
 
       {/* 3. "МАТЕРІАЛЬНІСТЬ" (Текстура полотна) */}
       {/* Додаємо дуже слабке зерно, щоб фон не здавався "пластиковим" */}
