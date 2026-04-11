a0dbbd9f8aab3c5266f6097649b9197cab140b45
C-H-P-P <kachandmytro20@gmail.com>
2026-04-11 22:19:37 +0300

    Fix gallery description fallback logic

--- src/utils/gallery.ts
+++ src/utils/gallery.ts
@@ -18,8 +18,8 @@ export function getGalleryAddress(gallery: Gallery, language: string): string {
 }
 
 export function getGalleryShortDescription(gallery: Gallery, language: string): string {
-    const descEn = gallery.description || gallery.description_en || gallery.short_description_en || '';
-    const descUa = gallery.description || gallery.description_ua || gallery.short_description_ua || '';
+    const descEn = gallery.description_en || gallery.short_description_en || gallery.description || '';
+    const descUa = gallery.description_ua || gallery.short_description_ua || gallery.description || '';
     return language === 'en' ? (descEn || descUa) : (descUa || descEn);
 }
 
